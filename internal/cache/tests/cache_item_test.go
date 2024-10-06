e84b103b13323fad07adead07eab362b095ec07a
Toolnado <toolnado@gmail.com>
2024-10-06 14:39:29 +0300

    test, refactor(cache): add new tests for CacheItem and refactor old

--- internal/cache/tests/cache_item_test.go
+++ internal/cache/tests/cache_item_test.go
@@ -6,17 +6,17 @@ import (
 )
 
 func TestNewCacheItem(t *testing.T) {
-	item := cache.NewCacheItem("hello", 5)
-	if item.Size != 5 {
+	item := cache.NewCacheItem([]byte("hello"))
+	if item.Size != int64(len("hello")) {
 		t.Errorf("invalid size value, expected: %d, received: %d\n", 5, item.Size)
 	}
-	if item.Value.(string) != "hello" {
+	if string(item.Value) != "hello" {
 		t.Errorf("invalid value, expected: %s, received: %s", "hello\n", item.Value)
 	}
 }
 
 func TestCacheItemUse(t *testing.T) {
-	item := cache.NewCacheItem("hello", 5)
+	item := cache.NewCacheItem([]byte("hello"))
 	item.Use()
 	if item.UsedCount.Load() != 1 {
 		t.Errorf("invalid userCount value, expected: %d, received: %d\n", 1, item.UsedCount.Load())
@@ -24,7 +24,7 @@ func TestCacheItemUse(t *testing.T) {
 }
 
 func TestCacheItemUsed(t *testing.T) {
-	item := cache.NewCacheItem("hello", 5)
+	item := cache.NewCacheItem([]byte("hello"))
 	item.Use()
 	if item.Used() != 1 {
 		t.Errorf("invalid userCount value, expected: %d, received: %d\n", 1, item.Used())
